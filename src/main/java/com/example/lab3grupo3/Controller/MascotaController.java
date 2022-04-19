e1d37382ffbbdc36cb6cd95730b1d2f3227b7e89
Alonso Rosales <a20191566@pucp.edu.pe>
2022-04-19 18:43:08 -0500

    commit base 1

---
+++ src/main/java/com/example/lab3grupo3/Controller/MascotaController.java
@@ -0,0 +1,12 @@
+package com.example.lab3grupo3.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Mascotas")
+public class MascotaController {
+
+
+
+}
