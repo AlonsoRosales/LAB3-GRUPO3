ad15ca425381d9996c843f470eb2be9464500607
Alonso Rosales <a20191566@pucp.edu.pe>
2022-04-19 18:39:56 -0500

    commit base

---
+++ src/test/java/com/example/lab3grupo3/Lab3Grupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3Grupo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
