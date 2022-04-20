17951838bc235c1420f0945d2fa241e21cde0977
avizcarra <a20181454@pucp.edu.pe>
2022-04-19 20:41:06 -0500

    Responsable funcionalidad completa

--- src/main/java/com/example/lab3grupo3/Controller/ResponsableController.java
+++ src/main/java/com/example/lab3grupo3/Controller/ResponsableController.java
@@ -3,19 +3,26 @@ package com.example.lab3grupo3.Controller;
 
 import com.example.lab3grupo3.Entity.Responsable;
 import com.example.lab3grupo3.Entity.Servicio;
+import com.example.lab3grupo3.Repository.ResponsableRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @Controller
 @RequestMapping(value = "Responsable")
 public class ResponsableController {
 
+    @Autowired
+    ResponsableRepository responsableRepository;
+
     @GetMapping(value = "lista")
     public String listar(Model model){
-        model.addAttribute("listaResponsables","");
+        model.addAttribute("listaResponsables",responsableRepository.findAll());
         return "responsables/list";
     }
 
@@ -30,8 +37,15 @@ public class ResponsableController {
     }
 
     @PostMapping(value = "guardar")
-    public String guardar(Responsable responsable) {
-        return "redirect:/Responsables/lista";
+    public String guardar(Responsable responsable, RedirectAttributes attr) {
+        if(responsable.getNombre().equalsIgnoreCase("")){
+            attr.addFlashAttribute("msg","No se ha ingresado el campo de nombre");
+            return "redirect:/Responsable/crear";
+        }else{
+            responsableRepository.save(responsable);
+            attr.addFlashAttribute("msg","Responsable agregado exitosamente");
+            return "redirect:/Responsable/lista";
+        }
     }
 
     @GetMapping(value = "eliminar")
